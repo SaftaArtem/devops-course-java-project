f20e181de664d16e0e1aa93f97b13830e63d42c9
ArtemSafta <jinnytrickz@gmail.com>
2026-07-06 12:10:38 +0300

    Add project to repo.
    - Add the .gitignore file to exclude unnecessary dirs.

---
+++ src/main/java/org/asafta/App.java
@@ -0,0 +1,13 @@
+package org.asafta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
