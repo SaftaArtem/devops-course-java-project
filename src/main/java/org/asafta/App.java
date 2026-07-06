291e7c7e0b95172074ccbdb2945b948f9ddb6e8a
ArtemSafta <jinnytrickz@gmail.com>
2026-07-06 20:06:25 +0300

    Change main file Make test compatible with junit 5

--- src/main/java/org/asafta/App.java
+++ src/main/java/org/asafta/App.java
@@ -6,8 +6,11 @@ package org.asafta;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static String getMessage() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
     }
 }
