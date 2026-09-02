020f35f2dc716bec677008a7c34abea0169bf345
ArtemSafta <jinnytrickz@gmail.com>
2026-09-02 19:40:01 +0300

    Add a comment in the main method

--- src/main/java/org/asafta/App.java
+++ src/main/java/org/asafta/App.java
@@ -15,6 +15,7 @@ public class App
     }
 
     public static void main(String[] args) {
+        // comment
         System.out.println(getMessage());
         System.out.println(getVersion());
     }
